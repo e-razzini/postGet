0d5e84c5b55042a7d53223445bfcee42932265e6
Elton <eltonrazzini@hotmail.com>
2020-04-14 23:28:12 -0300

    inicio projeto post

---
+++ cadastrar-usuario.php
@@ -0,0 +1,53 @@
+<?php
+
+$data = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+/*echo $data ['name'];
+echo "<br/>";
+echo $data ['E-mail'];
+echo "<br/>";
+echo $data ['login'];
+echo "<br/>";
+echo $data ['password'];
+
+foreach ($data as $key => $value) {
+    echo "<ul><li><?php echo "$value"?> </li>    </ul>";
+}
+*/
+?>
+<!DOCTYPE html>
+<html lang="pt-Br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="formulario">
+
+            <form method="post" action="">
+            <label for="name">name</label>
+          <p> <?php  echo $data['name'];?> </p>
+            <br/><br/>
+
+            <label for="E-mail">E-mail</label>
+           <p> <?php  echo $data['E-mail'];?> </p>
+            <br/><br/>
+
+            <label for="Login">login</label>
+            <p> <?php  echo $data['login'];?> </p>
+            <br/><br/>
+
+            <label for="password">password</label>
+            <p> <?php  echo $data['password'];?> </p>
+
+            <label for="password-Confirm">password Confirm</label>
+           <p> <?php  echo $data['Confirm'];?> </p>
+
+            <button type="submit" name="salvar" id="salvar">ok</button>
+
+        </form>
+        </div>
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css">
+        <title>cadastro</title>
+    </head>
+    <body>
+ <div class="formulario">
+
+            <form method="post" action="cadastrar-usuario.php">
+            <label for="name">name</label>
+            <input type="text" required name="name" id="name"/>
+            <br/><br/>
+
+            <label for="E-mail">E-mail</label>
+            <input type="text" required name="E-mail" id="E-mail" />
+            <br/><br/>
+
+            <label for="Login">login</label>
+            <input type="text" required name="login" id="login"  />
+            <br/><br/>
+
+            <label for="password">password</label>
+            <input type="password" required name="password" id="password" />
+
+            <label for="password-Confirm">password Confirm</label>
+            <input type="password" required name="Confirm" id="Confirm" />
+
+            <button type="submit" name="salvar" id="salvar">save</button>
+
+        </form>
+        </div>
+    </body>
+</html>
